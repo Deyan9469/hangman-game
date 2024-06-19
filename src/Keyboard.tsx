81dab0ad5b513adfeb8e8fcdcd47f8e25553bb81
Deyan9469 <deqnpavlikov@gmail.com>
2024-06-19 18:21:15 +0300

    fixing styles on screen keyboard

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -35,28 +35,36 @@ type KeyboardProps = {
     inactiveLetters: string[]
     addGuessedLetter: (letter: string) => void
 }
-export function Keyboard({ 
+export function Keyboard({
     activeLetters,
-    disabled = false,
     inactiveLetters,
-    addGuessedLetter }: KeyboardProps) {
-    return <div style={{
-        display: "grid",
-        gridTemplateColumns: "repeat (auto-fit, minmax(75px, 1fr))", gap: ".5rem",
-    }}
-    >
+    addGuessedLetter,
+    disabled = false,
+  }: KeyboardProps) {
+    return (
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+          gap: ".5rem",
+        }}
+      >
         {KEYS.map(key => {
-            const isActive = activeLetters.includes(key)
-            const isInactive = inactiveLetters.includes(key)
-            return (
-                <button 
-                onClick={() => addGuessedLetter(key)}
-                className={`${styles.btn} ${isActive ? styles.active : ""}
-                ${isInactive ? styles.inactive : ""}
-                `}
-                disabled={isInactive || isActive || disabled}
-                key={key}>{key}</button>
-            )
+          const isActive = activeLetters.includes(key)
+          const isInactive = inactiveLetters.includes(key)
+          return (
+            <button
+              onClick={() => addGuessedLetter(key)}
+              className={`${styles.btn} ${isActive ? styles.active : ""} ${
+                isInactive ? styles.inactive : ""
+              }`}
+              disabled={isInactive || isActive || disabled}
+              key={key}
+            >
+              {key}
+            </button>
+          )
         })}
-    </div>
-}
+      </div>
+    )
+  }
