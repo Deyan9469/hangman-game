c5de6a4532486978c745bfe371947683669e5fb9
Deyan9469 <deqnpavlikov@gmail.com>
2024-06-19 16:37:44 +0300

    adding left leg

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -82,6 +82,7 @@ export function HangmanDrawing() {
         {RIGHT_ARM}
         {LEFT_ARM}
         {RIGHT_LEG}
+        {LEFT_LEG}
         <div style={{ height: "50px", width: "10px", background: "black", position: "absolute", top: 0, right: 0, }} />
         <div style={{ height: "100px", width: "200px", background: "black", marginLeft: "120px" }} />
         <div style={{ height: "400px", width: "10px", background: "black", marginLeft: "120px" }} />
