b26ea4d9ff22a2a83f47dd20780874d22078c49e
Deyan9469 <deqnpavlikov@gmail.com>
2024-06-19 12:24:25 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { HangmanWord } from "./HangmanWord";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json"
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  })
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  return <div style={{
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+    margin: "0 auto",
+    alignItems: "center"
+  }}
+  >
+    <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+    <HangmanDrawing />
+    <HangmanWord />
+    <Keyboard />
+  </div>
+}
+
+export default App
---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,6 @@
+export function HangmanDrawing() {
+    return <div style={{position: "relative"}}>
+        <div style={{height:"10px", width: "250px", background: "black"}}/>
+        
+    </div>
+}
